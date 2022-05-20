0c8fa91f017ede2eeca8dacda554b879424c3e9c
MaysLastPlay0 <105645266+MaysLastPlay0@users.noreply.github.com>
2022-05-20 16:21:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Sunday Night Suicide Android Port
+Official Port By MaysLastPlay 
+
+Mod By The_Mayz And His Team
+Zoe.exe Shake Warning
+
---
+++ assets/preload/stages/street1.lua
@@ -0,0 +1,29 @@
+function onCreate()
+	-- background shit
+	makeLuaSprite('street1', 'street1', -500, -300);
+	setLuaSpriteScrollFactor('street1', 0.9, 0.9);
+	
+	makeLuaSprite('stagefront', 'stagefront', -650, 600);
+	setLuaSpriteScrollFactor('stagefront', 0.9, 0.9);
+	scaleObject('stagefront', 1.1, 1.1);
+
+	-- sprites that only load if Low Quality is turned off
+	if not lowQuality then
+		makeLuaSprite('stagelight_left', 'stage_light', -125, -100);
+		setLuaSpriteScrollFactor('stagelight_left', 0.9, 0.9);
+		scaleObject('stagelight_left', 1.1, 1.1);
+		
+		makeLuaSprite('stagelight_right', 'stage_light', 1225, -100);
+		setLuaSpriteScrollFactor('stagelight_right', 0.9, 0.9);
+		scaleObject('stagelight_right', 1.1, 1.1);
+		setPropertyLuaSprite('stagelight_right', 'flipX', true); --mirror sprite horizontally
+
+		makeLuaSprite('stagecurtains', 'stagecurtains', -500, -300);
+		setLuaSpriteScrollFactor('stagecurtains', 1.3, 1.3);
+		scaleObject('stagecurtains', 0.9, 0.9);
+	end
+
+	addLuaSprite('street1', false);
+	
+	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
+end
