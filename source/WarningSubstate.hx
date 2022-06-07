b06fd49cdd83d6a74d6c669369099f38556003d3
L <you@example.com>
2022-06-07 15:04:21 -0400

    i forgor

--- source/WarningSubstate.hx
+++ source/WarningSubstate.hx
@@ -11,7 +11,7 @@ import flixel.addons.transition.FlxTransitionableState;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
 
-class FlashingState extends MusicBeatState
+class WarningSubstate extends MusicBeatState
 {
 	public static var leftState:Bool = false;
 
