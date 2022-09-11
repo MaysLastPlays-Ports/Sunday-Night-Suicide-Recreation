b9eb56933b00e329e534f41844908229bbc11446
MaysLastPlay0 <105645266+MaysLastPlay0@users.noreply.github.com>
2022-09-11 18:20:06 +0300

    update ShakingWarningSubState.hx

--- source/ShakingWarningSubState.hx
+++ source/ShakingWarningSubState.hx
@@ -17,7 +17,7 @@ class ShakingWarningSubState extends MusicBeatSubstate
 		warningtext = new FlxText(0 + 300, 0 + 300, 0, "", 32);
 		#if android
 		warningtext2 = new FlxText(0 + 130, 0 + 350, 0, "Press A to Procceed, Press B to turn shaking off", 32);
-		#else if windows
+		#else
 		warningtext2 = new FlxText(0 + 130, 0 + 350, 0, "Press Enter to Procceed, Press CTRL to turn shaking off", 32);
 				#end
 		if(PlayState.isStoryMode && ClientPrefs.shaking) {
