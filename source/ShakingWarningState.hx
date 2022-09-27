e382f36e1d324915150ec37a6132da8178d269a4
MaysLastPlay0 <105645266+MaysLastPlay0@users.noreply.github.com>
2022-09-27 10:56:41 +0300

    update ShakingWarningState.hx

--- source/ShakingWarningState.hx
+++ source/ShakingWarningState.hx
@@ -11,7 +11,7 @@ import flixel.addons.transition.FlxTransitionableState;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxTimer;
 
-class ShakeWarningState extends MusicBeatState
+class ShakingWarningState extends MusicBeatState
 {
 	public static var leftState:Bool = false;
 
